0754fe19a881c19e1f0f2d2900dbb14e24711317
apenny020 <apenny020@gmail.com>
2023-03-09 14:45:30 +0000

    changed the code to be more efficient and can officially now do a dictionary in a list, just need to scale to all lines

--- read_data.py
+++ read_data.py
@@ -8,6 +8,15 @@
 #think about how youre going to use data actually
 import re
 
+
+#alternate idea
+#read in lines into list
+#have a dictionary of the 4 words
+#create a list that contains multiple valueless dictionaries for the length of the first list
+#append into the dictionary with the values
+
+
+
 #read in data and put it into a list with lines being seperated by commas
 data_list = [] # the list of all the lines
 dataFile = open("data_input_file_2018.txt", "r", encoding='utf-8-sig')
@@ -19,55 +28,31 @@ print (data_list[0])
 
 
 patient_list = [] #??????
+data_list_new = []
 c = 0 # counter
-test_item = [] #for testing - just using the first line - replace to loop through each line later
+test_item = [] #for testing - just using the first line - replace to loop through each line later, use data_list
 test_item.append(data_list[0])
+#test_item.append(data_list[1])
+#test_item.append(data_list[2])
 
 for i in test_item: #CHANGE TO ACTUAL LIST, this is where it will loop through each line
   item = i
   item = item.split("\t") # now a list in list
+  print(item)
   print("PRINT C")
   print(c)
-  c = 1 #change to incremental, if its event needed anymore?
 
   #adding the names of keys
   dictionary_names_list = ["unique_identifier", "date_time", "state_enter_ignore", "action"]
-  index_counter = 0 # counts through the index at which point to add the string
-  list_counter = 0 # counts for index in dictionary_names_list
-  for i in dictionary_names_list:
-    item.insert(index_counter, dictionary_names_list[list_counter]) #inserts the dictionary key name evey other item
-    index_counter += 2
-    list_counter += 1
-    print(item)
-    print(list_counter)
-    print(i)
+  
+  data_list_new.append(dict(zip(dictionary_names_list, item))) # should create a dictionary zipped up
+  print(data_list_new)
 
+  c = 1 #change to incremental, if its event needed anymore?
 
-
-  #convert to tuples
-  print("HERE")
-  #it = iter(item)
-  #it(zip(it, it))
-
-  # list of vowels
-  #phones = ['apple', 'samsung', 'oneplus']
-  #phones_iter = iter(item)
-
-  #print(next(phones_iter))   
-  #print(next(phones_iter)) 
-  #print(next(phones_iter))    
+  
 
 
   
 
-  #patient_list.append(item) #line now in the list
-
-  #converting to dictionary
-
-  #patient_list[c] = {}
-  #c2 = 0 # second counter
-  #for i in item:
-  #  patient_list.update({c2:i})
-  #  unique_identifier = patient_list[c2] 
-  #  c2 += 1
     
