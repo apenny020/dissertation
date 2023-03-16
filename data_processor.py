a6d1d093f5ffe6e1b8087d5a2550d9d1c5069bc9
apenny020 <apenny020@gmail.com>
2023-03-16 15:24:24 +0000

    Started work on the function for looping to find wait times

--- data_processor.py
+++ data_processor.py
@@ -7,6 +7,102 @@
 #also be good to know how many people go on each pathway
 
 
+#for each day
+#multiple patients to keep track of
+
+#could do average of times, but i think a distribution would be more accurate
+#for distribution - record all times and then order them, then you can take length divided by x to get a certain proportion of the distribution
 
 
+
+#need a temp collector
+#for each day
+#for each patient
+#work out waiting times
+
+def calculating_wait_times (data_list):
+    #List of waiting times in minutes
+    wait_for_Height_and_weight = ()
+    wait_for_Bloods = ()
+    wait_for_consultation_1 = ()
+    wait_for_consultation_2 = () #combine with above?
+
+
+    temp_val = []
+    temp_val.append(data_list(0))
+    current_day = temp_val.get("date_time")      
+    current_day = current_day[:10] #should cut it to just be date
+    
+    #needs to do a loop, while date_time == current_day
+    #store patient in patient dict
+    todays_patients = []
+        #this occurs when appointed
+        todays_patients.append(unique_identifier)
+
+    #then grab timestamp
+
+    #eventually has to loop round and find the next day somehow
+    
+    pass
+
+
+
+
+
+
+
+#further work - different days could have different waiting times because of different number of staff - so adjust to this
+# ^ would play into changing number of staff
+
+
+#need to access the sorted list from data processor
+#then seperate the information into dates (each day is 1 list of dictionary<-- double think this data structure)
+#then 2 tasks
+
+#1 reorder data by patient number
+#work out how long they waited for each thing
+#store this in the main data structure
+
+#2 - extension see if you can work out how many staff working by how many patients in which area at the same time
+
+#also need a main data structure in here that is maybe a dictionary...
+#how likely to use kiosk or reception
+#how likely to DNA
+#how likely to be late - and by how much
+#time from check in to weight and height 
+#time from check in to other if identified?
+#time from 
+
+#Possible states are
+#Appointed
+#Patient Identified by Kiosk
+#Waiting Height and Weight
+#Waiting Consultation 1 of 1
+#In Consultation 1 of 1
+#Waiting Consultation 1 of 2
+#In Consultation 2 of 2
+#Waiting Blood Room
+#In Blood Room
+#Complete
+#Complete, Bloods Done
+#Cancelled
+#Late Arrival
+#DNA
+
+#Patients can come in for
+#Height weight, consultation (1or2)
+#Height weight, consultation (1or2), Bloods
+#Bloods
+#Cancelled
+#Late arrival, DNA
+
+
+#Exclusions
+#If does not complete - ignore data, 
+#if more than 10 hours, ignore
+
+#make sure you change to all upper or all lower case
+
+#need to reread the data and look at how its set for this ^
+#need to read about how to make probablilities - are they going to be purely from the data, how work out - write it more
 
--- read_data.py
+++ read_data.py
@@ -8,38 +8,53 @@
 #think about how youre going to use data actually
 import re
 
-class read_data:
-  def read_in_data(): # could change parameter to take in x text file
-    #read in data and put it into a list with lines being seperated by commas
-    input_data_list = [] # the list of all the lines
-    dataFile = open("data_input_file_2018.txt", "r", encoding='utf-8-sig')
-    for line in dataFile:
-      whole_line = line
-      input_data_list.append(whole_line)
-    dataFile.close()
-    print (input_data_list[0])
+
+def read_in_data(): # could change parameter to take in x text file
+  #read in data and put it into a list with lines being seperated by commas
+  input_data_list = [] # the list of all the lines
+  dataFile = open("data_input_file_2018.txt", "r", encoding='utf-8-sig')
+  for line in dataFile:
+    whole_line = line
+    input_data_list.append(whole_line)
+  dataFile.close()
+  print (input_data_list[0])
 
 
-    data_list = []
-    #test_item = [] #for testing - just using the first line - replace to loop through each line later, use data_list
-    #test_item.append(data_list[0])
+  data_list = []
+  #test_item = [] #for testing - just using the first line - replace to loop through each line later, use data_list
+  #test_item.append(data_list[0])
 
-    for i in input_data_list: #CHANGE TO ACTUAL LIST, this is where it will loop through each line
-      item = i
-      item = item.split("\t") # now a list in list
+  for i in input_data_list: #CHANGE TO ACTUAL LIST, this is where it will loop through each line
+    item = i
+    item = item.split("\t") # now a list in list
 
-      #adding the names of keys
-      dictionary_names_list = ["unique_identifier", "date_time", "state_enter_ignore", "action"]
-      
-      data_list.append(dict(zip(dictionary_names_list, item))) # should create a dictionary zipped up
-   
-    return (data_list)
+    #adding the names of keys
+    dictionary_names_list = ["unique_identifier", "date_time", "state_enter_ignore", "action"]
+    
+    data_list.append(dict(zip(dictionary_names_list, item))) # should create a dictionary zipped up
+  
+  
+  return (data_list)
+
+
+#next function - the stuff needs to be organised by date
+#this is so that we can track the actions in order and then can measure time and work day by day
+#later we will cater for exception cases of if the data is abonormal or x
+
+#function to sort list by date 
+def sort_data_by_date(unsorted_list):
+  sorted_list = sorted(unsorted_list, key=lambda d: d['date_time'])
+  print("done")
+  return (sorted_list)
+
+#next, move data to data_processor
+
 
 data_lsit = []
-data_list = read_data()
-print(data_list)
-
-
+data_list = read_in_data() #get list of data
+date_sorted_data_list = sort_data_by_date(data_list) #sort data by date
+print(date_sorted_data_list)
   
 
-    
+#IMPORTANT NOTE   
+#above has sorted but does not keep the same patient together - keep that in mind
