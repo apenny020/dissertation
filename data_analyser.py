895d411b45f5a8855a3f8534115b71ce0b54ca2b
apenny020 <apenny020@gmail.com>
2023-04-14 16:11:50 +0100

    added the probability maker

--- data_analyser.py
+++ data_analyser.py
@@ -32,7 +32,36 @@ def order_list(to_order_list, file_name):
         f.close()
     return (ordered_list)
 
-#create distributions to be used
+#create distributions to be used (for one day)
+#first for time based data - waiting times/durations/lates
+#also
+#second for number of patients 
+#the list should be a number over multiple dates
+def get_probabilities_time(data_list):
+    tally_dict = {}
+    ordered_list = sorted(data_list)
+    for i in ordered_list:
+        if i in tally_dict:
+            #increase by value 1
+            temp_val = tally_dict[i]
+            temp_val += 1
+            tally_dict.update({i:temp_val})
+        else:
+            #add to dict
+            tally_dict[i] = 1
+        
+    total = len(ordered_list)
+    for i in tally_dict:
+        #get value and put in probability
+        #update value to this
+        temp_val = tally_dict[i]
+        temp_val = temp_val/total
+        tally_dict.update({i:temp_val})
+    
+    return (tally_dict)
+    
+
+
 
 
 #workout appointment times?
