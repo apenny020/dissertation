146401d11233d7af59148403ed255260dea81ac2
apenny020 <apenny020@gmail.com>
2023-04-15 18:54:45 +0100

    added comments to layout/put the next things to be added

--- omni_system.py
+++ omni_system.py
@@ -6,3 +6,66 @@
 #universal 'time' measure
 ticks = 1440 #minutes in a day
 global ticks
+
+
+######################################################
+#Pre model stuff 
+
+#Function
+#Unpacks dictionary of lists of data from data processing 
+#and sends it to data analysis for percentage calculation and charts
+
+
+
+#Function to get patients and workflow for the day
+
+#initialise
+#work out how many patients there will be today
+#work out how many consultants there is supposed be today
+#work out how many nurses there is supposed to be today
+##clinic opening and closing times
+
+#a list of appointment times for each consultant and for bloods
+# ^ appointment times x mins apart and bloods x mins apart
+
+#work out which consultants sick if any
+#Update patients dictionary with appointments cancelled
+
+
+#go through each patient and assign them a workflow
+# ^ if they have an appointment, take that appointment time off the list for that consultant
+# ^ write when tick they're meant to get called through
+
+##########################
+
+#Functions that get called every tick
+#Checking to see if a patient needs to move on
+#Update new patient dictionary with when they were actually called through
+#Check if nurse is free
+# ^if yes then call next waiting through (just the one waiting the longest)
+# ^if no then ignore
+#Check if consultant is free
+# ^if yes then call the next closest to their appointment time (but making sure that its not ignoring the ones that have been waiting)
+# ^^eg if you have a 16:00 patient waiting and 16:30 patient waiting and its 16:35 - make sure still pick the 16:00 patient
+#Update patients patient satisfaction
+
+
+###########################
+#Recording data
+#Records waiting times for each action
+#Records duration for each action
+#Records total number of
+#^patients
+#^nurses
+#^consultants
+#^DNA's
+#^Lates
+#Records total number of patients in:
+#^Consultation at any one time
+#^H&W&Bloods at any one time
+#^Clinic at any one time
+#Records total time in the clinic
+
+
+####################################
+
